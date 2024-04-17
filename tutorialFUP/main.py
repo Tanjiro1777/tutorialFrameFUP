73041d32f3be4d6e48e56a5052c7bb913f3d2a0d
SebastianNeo <sebastiangear2018@gmail.com>
2024-04-16 21:42:58 -0500

    Implementacion en metodo main para funcion de estudiante

--- tutorialFUP/main.py
+++ tutorialFUP/main.py
@@ -5,20 +5,66 @@ from flask import request
 from flask_cors import CORS
 import json
 from waitress import serve
+from Controladores.ControladorEstudiante import ControladorEstudiante
+
 
 app = Flask(__name__)
 cors = CORS(app)
+
+
+#Implementacion de los metodos
+miControladorEstudiante=ControladorEstudiante()
 
 #Servicio que usaremos, funcion de Python que sirve para
 #Usaremos el Crud
 #Primer elemento a usar se usara un elemento de flask, de la siguiente manera:
 #En la linea de app.route es el camino que se toma para ejecutar cierta accion,todo se hace a traves de la url
 
+
 @app.route("/", methods=['GET'])
 def test ():
     json = {}
-    json["message"] = "Server Running..." #Esto hay que convertir una variable de tipo json
+    json["message"] = "Server Running..." #Esto h
+    # ay que convertir una variable de tipo json
     return jsonify(json)
+
+################################################################
+
+@app.route("/estudiantes", methods=['GET'])
+def getEstudiantes():
+    json = miControladorEstudiante.index()
+    return jsonify(json)
+
+
+@app.route("/estudiantes", methods=['POST'])
+def crearEstudiante():
+    data = request.get_json()
+    json = miControladorEstudiante.create(data)
+    return jsonify(json)
+
+
+@app.route("/estudiantes/<string:id>", methods=['GET'])
+def getEstudiante(id):
+    json = miControladorEstudiante.show(id)
+    return jsonify(json)
+
+
+@app.route("/estudiantes/<string:id>", methods=['PUT'])
+def modificarEstudiante(id):
+    data = request.get_json()
+    json = miControladorEstudiante.update(id, data)
+    return jsonify(json)
+
+
+@app.route("/estudiantes/<string:id>", methods=['DELETE'])
+def eliminarEstudiante(id):
+    json = miControladorEstudiante.delete(id)
+    return jsonify(json)
+
+
+#####################################################################
+
+
 
 def loadFileConfig():
     with open('config.json') as f:
